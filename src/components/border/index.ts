42506e53beafe93098955bfe5ed3135602122a6f
shy1118 <shy1118999@qq.com>
2024-02-01 13:34:14 +0800

    feat: export

--- src/components/border/index.ts
+++ src/components/border/index.ts
@@ -1,7 +1,29 @@
 import BoxBorder01 from './BoxBorder01/index.vue'
 import BoxBorder02 from './BoxBorder02/index.vue'
+import BoxBorder03 from './BoxBorder03/index.vue'
+import BoxBorder04 from './BoxBorder04/index.vue'
+import BoxBorder05 from './BoxBorder05/index.vue'
+import BoxBorder06 from './BoxBorder06/index.vue'
+import BoxBorder07 from './BoxBorder07/index.vue'
+import BoxBorder08 from './BoxBorder08/index.vue'
+import BoxBorder09 from './BoxBorder09/index.vue'
+import BoxBorder10 from './BoxBorder10/index.vue'
+import BoxBorder11 from './BoxBorder11/index.vue'
+import BoxBorder12 from './BoxBorder12/index.vue'
+import BoxBorder13 from './BoxBorder13/index.vue'
 
 export {
   BoxBorder01,
   BoxBorder02,
+  BoxBorder03,
+  BoxBorder04,
+  BoxBorder05,
+  BoxBorder06,
+  BoxBorder07,
+  BoxBorder08,
+  BoxBorder09,
+  BoxBorder10,
+  BoxBorder11,
+  BoxBorder12,
+  BoxBorder13,
 }
--- src/components/decoration/index.ts
+++ src/components/decoration/index.ts
@@ -1,4 +1,27 @@
 import Decoration01 from './Decoration01/index.vue'
 import Decoration02 from './Decoration02/index.vue'
+import Decoration03 from './Decoration03/index.vue'
+import Decoration04 from './Decoration04/index.vue'
+import Decoration05 from './Decoration05/index.vue'
+import Decoration06 from './Decoration06/index.vue'
+import Decoration07 from './Decoration07/index.vue'
+import Decoration08 from './Decoration08/index.vue'
+import Decoration09 from './Decoration09/index.vue'
+import Decoration10 from './Decoration10/index.vue'
+import Decoration11 from './Decoration11/index.vue'
+import Decoration12 from './Decoration12/index.vue'
 
-export { Decoration01, Decoration02 }
+export {
+  Decoration01,
+  Decoration02,
+  Decoration03,
+  Decoration04,
+  Decoration05,
+  Decoration06,
+  Decoration07,
+  Decoration08,
+  Decoration09,
+  Decoration10,
+  Decoration11,
+  Decoration12,
+}
